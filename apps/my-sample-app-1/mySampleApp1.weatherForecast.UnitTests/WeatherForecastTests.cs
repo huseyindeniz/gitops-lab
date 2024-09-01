f8c4feb6526fb2a5b16e65b6fa87abe6160c65fb
Hüseyin Deniz KIVRAK <huseyindenizkivrak@gmail.com>
2024-09-01 15:19:36 +0300

    Mark mySampleApp1-weatherforecast unit tests. (#1)
    * mark weatherforecast unit tests.

--- apps/my-sample-app-1/mySampleApp1.weatherForecast.UnitTests/WeatherForecastTests.cs
+++ apps/my-sample-app-1/mySampleApp1.weatherForecast.UnitTests/WeatherForecastTests.cs
@@ -2,6 +2,8 @@ using mySampleApp1.weatherForecast.Domain;
 
 namespace mySampleApp1.weatherForecast.UnitTests
 {
+    [Trait("Category", "Unit")]
+    [Trait("Microservice", "weatherForecast")]
     public class WeatherForecastTests
     {
         [Fact]
