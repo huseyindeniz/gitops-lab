1d1ba58db19bedc8be8544e820eeb35f68d3f28e
Hüseyin Deniz KIVRAK <huseyindenizkivrak@gmail.com>
2024-09-30 23:53:42 +0300

    Deploy with argo (#4)
    * add app of apps
    
    * fix things.

--- terraform/argocd/app-of-apps.tf
+++ terraform/argocd/app-of-apps.tf
@@ -1,7 +1,8 @@
 resource "helm_release" "argo_cd_local_app_of_apps" {
   name       = "argo-cd-local-app-of-apps"
-  repository = "https://github.com/huseyindeniz/cicd-lab/helm-charts"
+  repository = "../../helm-charts"
   chart      = "app-of-apps"
+  version    = "main"
   namespace  = kubernetes_namespace.argo_cd_local.metadata[0].name
   values     = [file("${path.module}/values/app-of-apps-values.yaml")]
 }
