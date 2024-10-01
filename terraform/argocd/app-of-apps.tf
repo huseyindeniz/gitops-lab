9b8538b587bf93634659b853c44e80b3cdbfa20b
Hüseyin Deniz KIVRAK <huseyindenizkivrak@gmail.com>
2024-10-01 12:55:00 +0300

    fix app of apps.

--- terraform/argocd/app-of-apps.tf
+++ terraform/argocd/app-of-apps.tf
@@ -1,8 +1,28 @@
-resource "helm_release" "argo_cd_local_app_of_apps" {
-  name       = "argo-cd-local-app-of-apps"
-  repository = "../../helm-charts"
-  chart      = "app-of-apps"
-  version    = "main"
-  namespace  = kubernetes_namespace.argo_cd_local.metadata[0].name
-  values     = [file("${path.module}/values/app-of-apps-values.yaml")]
-}
+# resource "kubernetes_manifest" "app_of_apps" {
+#   manifest = {
+#     "apiVersion" = "argoproj.io/v1alpha1"
+#     "kind"       = "Application"
+#     "metadata" = {
+#       "name"      = "app-of-apps"
+#       "namespace" = kubernetes_namespace.argo_cd_local.metadata[0].name
+#     }
+#     "spec" = {
+#       "project" = "default"
+#       "source" = {
+#         "repoURL"        = "https://github.com/huseyindeniz/cicd-lab.git"
+#         "targetRevision" = "main"
+#         "path"           = "helm-charts/appset"
+#       }
+#       "destination" = {
+#         "server"    = "https://kubernetes.default.svc"
+#         "namespace" = kubernetes_namespace.argo_cd_local.metadata[0].name
+#       }
+#       "syncPolicy" = {
+#         "automated" = {
+#           "prune"    = true
+#           "selfHeal" = true
+#         }
+#       }
+#     }
+#   }
+# }
