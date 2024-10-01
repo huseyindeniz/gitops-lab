f93eac5f568454dd1cdb3845f0cd54100b3770cc
Hüseyin Deniz KIVRAK <huseyindenizkivrak@gmail.com>
2024-10-01 13:01:41 +0300

    fix app of apps.

--- terraform/argocd/app-of-apps.tf
+++ terraform/argocd/app-of-apps.tf
@@ -1,28 +1,28 @@
-# resource "kubernetes_manifest" "app_of_apps" {
-#   manifest = {
-#     "apiVersion" = "argoproj.io/v1alpha1"
-#     "kind"       = "Application"
-#     "metadata" = {
-#       "name"      = "app-of-apps"
-#       "namespace" = kubernetes_namespace.argo_cd_local.metadata[0].name
-#     }
-#     "spec" = {
-#       "project" = "default"
-#       "source" = {
-#         "repoURL"        = "https://github.com/huseyindeniz/cicd-lab.git"
-#         "targetRevision" = "main"
-#         "path"           = "helm-charts/appset"
-#       }
-#       "destination" = {
-#         "server"    = "https://kubernetes.default.svc"
-#         "namespace" = kubernetes_namespace.argo_cd_local.metadata[0].name
-#       }
-#       "syncPolicy" = {
-#         "automated" = {
-#           "prune"    = true
-#           "selfHeal" = true
-#         }
-#       }
-#     }
-#   }
-# }
+resource "kubernetes_manifest" "app_of_apps" {
+  manifest = {
+    "apiVersion" = "argoproj.io/v1alpha1"
+    "kind"       = "Application"
+    "metadata" = {
+      "name"      = "app-of-apps"
+      "namespace" = kubernetes_namespace.argo_cd_local.metadata[0].name
+    }
+    "spec" = {
+      "project" = "default"
+      "source" = {
+        "repoURL"        = "https://github.com/huseyindeniz/cicd-lab.git"
+        "targetRevision" = "main"
+        "path"           = "helm-charts/appset"
+      }
+      "destination" = {
+        "server"    = "https://kubernetes.default.svc"
+        "namespace" = kubernetes_namespace.argo_cd_local.metadata[0].name
+      }
+      "syncPolicy" = {
+        "automated" = {
+          "prune"    = true
+          "selfHeal" = true
+        }
+      }
+    }
+  }
+}
