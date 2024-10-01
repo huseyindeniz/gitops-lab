d25ef664664f2a91481cbcccc5e5145850db4c97
Hüseyin Deniz KIVRAK <huseyindenizkivrak@gmail.com>
2024-10-01 17:37:18 +0300

    fix app of apps.

--- terraform/argocd/app-of-apps.tf
+++ terraform/argocd/app-of-apps.tf
@@ -11,7 +11,7 @@ resource "kubernetes_manifest" "app_of_apps" {
       "source" = {
         "repoURL"        = "https://github.com/huseyindeniz/cicd-lab.git"
         "targetRevision" = "main"
-        "path"           = "helm-charts/appsets"
+        "path"           = "manifests"
         "directory" = {
           "recurse" = true
         }
