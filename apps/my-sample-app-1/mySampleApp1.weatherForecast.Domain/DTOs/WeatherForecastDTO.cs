28b5b4c79888e56c0552c7c91688d83223c77463
Hüseyin Deniz KIVRAK <huseyindenizkivrak@gmail.com>
2024-10-03 18:05:58 +0300

    prepare webapi project for ef core migration with argocd hooks.

--- apps/my-sample-app-1/mySampleApp1.weatherForecast.Domain/DTOs/WeatherForecastDTO.cs
+++ apps/my-sample-app-1/mySampleApp1.weatherForecast.Domain/DTOs/WeatherForecastDTO.cs
@@ -8,7 +8,7 @@
 
         public int TemperatureC { get; private set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(TemperatureC * 9 / 5);
 
         public string? Summary { get; private set; }
     }
