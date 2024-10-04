155f86a757d2915e81ad3236c818ff97a7c630f1
Hüseyin Deniz KIVRAK <huseyindenizkivrak@gmail.com>
2024-10-04 13:19:10 +0300

    add multiple staging env support to project-001.

--- terraform/argocd/namespaces.tf
+++ terraform/argocd/namespaces.tf
@@ -16,6 +16,24 @@ resource "kubernetes_namespace" "project_001_weather_forecast_staging" {
   }
 }
 
+resource "kubernetes_namespace" "project_001_weather_forecast_stag_1" {
+  metadata {
+    name = "project-001-weather-forecast-stag-1"
+  }
+}
+
+resource "kubernetes_namespace" "project_001_weather_forecast_stag_2" {
+  metadata {
+    name = "project-001-weather-forecast-stag-2"
+  }
+}
+
+resource "kubernetes_namespace" "project_001_weather_forecast_stag_3" {
+  metadata {
+    name = "project-001-weather-forecast-stag-3"
+  }
+}
+
 # resource "kubernetes_namespace" "monitoring" {
 #   metadata {
 #     name = "monitoring"
