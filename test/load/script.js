fffcc46ed7ab338d0cc11d25b2682c6526b31f76
Hüseyin Deniz KIVRAK <huseyindenizkivrak@gmail.com>
2024-10-26 18:48:46 +0300

    fix hpa

--- test/load/script.js
+++ test/load/script.js
@@ -3,7 +3,7 @@ import { sleep } from 'k6';
 
 export const options = {
   // A number specifying the number of VUs to run concurrently.
-  vus: 250,
+  vus: 150,
   // A string specifying the total duration of the test run.
   duration: '2m',
 
