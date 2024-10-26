fed4ac5e6eb65e3e68b10a78364fb15121d01892
Hüseyin Deniz KIVRAK <huseyindenizkivrak@gmail.com>
2024-10-26 12:21:56 +0300

    fix attempt for flux auto image update.

--- terraform/local/flux.tf
+++ terraform/local/flux.tf
@@ -6,7 +6,7 @@
 # }
 
 resource "flux_bootstrap_git" "flux_bootstrap" {
-  embedded_manifests = false
+  embedded_manifests = true
   path               = var.flux_path
   components_extra   = ["image-reflector-controller", "image-automation-controller"]
 }
