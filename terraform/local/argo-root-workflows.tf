01196490a8ba59ef4d9e1af54c81522ab518a433
Hüseyin Deniz KIVRAK <huseyindenizkivrak@gmail.com>
2024-11-21 21:06:08 +0200

    create hello-workflows workflow.

---
+++ terraform/local/argo-root-workflows.tf
@@ -0,0 +1,31 @@
+resource "kubernetes_manifest" "argo_root_workflows" {
+  manifest = {
+    "apiVersion" = "argoproj.io/v1alpha1"
+    "kind"       = "Application"
+    "metadata" = {
+      "name"      = "argo-root-workflows"
+      "namespace" = kubernetes_namespace.argo_cd_local.metadata[0].name
+    }
+    "spec" = {
+      "project" = "default"
+      "source" = {
+        "repoURL"        = "https://github.com/huseyindeniz/cicd-lab.git"
+        "targetRevision" = "main"
+        "path"           = "${var.flux_path}/workflows"
+        "directory" = {
+          "recurse" = true
+        }
+      }
+      "destination" = {
+        "server"    = "https://kubernetes.default.svc"
+        "namespace" = kubernetes_namespace.argo_cd_local.metadata[0].name
+      }
+      "syncPolicy" = {
+        "automated" = {
+          "prune"    = true
+          "selfHeal" = true
+        }
+      }
+    }
+  }
+}
