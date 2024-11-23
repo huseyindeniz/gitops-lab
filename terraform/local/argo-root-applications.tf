207b05d6309a7cc216843c34d4872dfcb205af7a
Hüseyin Deniz KIVRAK <huseyindenizkivrak@gmail.com>
2024-11-23 19:54:43 +0200

    remove flux.

--- terraform/local/argo-root-applications.tf
+++ terraform/local/argo-root-applications.tf
@@ -1,32 +1,32 @@
-resource "kubernetes_manifest" "argo_root_applications" {
-  manifest = {
-    "apiVersion" = "argoproj.io/v1alpha1"
-    "kind"       = "Application"
-    "metadata" = {
-      "name"      = "argo-root-applications"
-      "namespace" = var.argo_namespace
-    }
-    "spec" = {
-      "project" = "default"
-      "source" = {
-        "repoURL"        = "https://github.com/huseyindeniz/cicd-lab.git"
-        "targetRevision" = "main"
-        "path"           = "${var.flux_path}/applications"
-        "directory" = {
-          "recurse" = true
-        }
-      }
-      "destination" = {
-        "server"    = "https://kubernetes.default.svc"
-        "namespace" = var.argo_namespace
-      }
-      "syncPolicy" = {
-        "automated" = {
-          "prune"    = true
-          "selfHeal" = true
-        }
-      }
-    }
-  }
-  depends_on = [module.local_argo, flux_bootstrap_git.flux_bootstrap]
-}
+# resource "kubernetes_manifest" "argo_root_applications" {
+#   manifest = {
+#     "apiVersion" = "argoproj.io/v1alpha1"
+#     "kind"       = "Application"
+#     "metadata" = {
+#       "name"      = "argo-root-applications"
+#       "namespace" = var.argo_namespace
+#     }
+#     "spec" = {
+#       "project" = "default"
+#       "source" = {
+#         "repoURL"        = "https://github.com/huseyindeniz/cicd-lab.git"
+#         "targetRevision" = "main"
+#         "path"           = "${var.flux_path}/applications"
+#         "directory" = {
+#           "recurse" = true
+#         }
+#       }
+#       "destination" = {
+#         "server"    = "https://kubernetes.default.svc"
+#         "namespace" = var.argo_namespace
+#       }
+#       "syncPolicy" = {
+#         "automated" = {
+#           "prune"    = true
+#           "selfHeal" = true
+#         }
+#       }
+#     }
+#   }
+#   depends_on = [module.local_argo.argo_cd_release, flux_bootstrap_git.flux_bootstrap]
+# }
--- terraform/modules/argo/outputs.tf
+++ terraform/modules/argo/outputs.tf
@@ -1,10 +1,23 @@
 # outputs.tf for Argo CD
 
+output "argo_cd_release" {
+  value = helm_release.argo_cd
+}
+
+output "argo_rollouts_release" {
+  value = helm_release.argo_rollouts
+}
+
+output "argo_workflows_release" {
+  value = helm_release.argo_workflows
+}
+
 # Output the Argo CD admin password
 output "argo_cd_admin_password" {
   description = "The Argo CD initial admin password"
   value       = data.kubernetes_secret.argo_cd_admin_secret.data["password"]
   sensitive   = true
+  depends_on  = [helm_release.argo_cd]
 }
 
 
@@ -13,6 +26,8 @@ output "argo_workflows_service_account_token" {
   description = "The Argo Workflows token"
   value       = data.kubernetes_secret.argo_workflows_token.data["token"]
   sensitive   = true
+
+  depends_on = [helm_release.argo_workflows]
 }
 
 
