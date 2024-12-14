e60bdf3861c7bd6951bf3f0abbcd37cc36d2f808
Hüseyin Deniz KIVRAK <huseyindenizkivrak@gmail.com>
2024-12-14 18:01:23 +0200

    restructure tests.

--- apps/my-sample-app-1/dotnet-services/mySampleApp1.wf.tests.unit/ci-test-run.sh
+++ apps/my-sample-app-1/dotnet-services/mySampleApp1.wf.tests.unit/ci-test-run.sh
@@ -6,8 +6,9 @@ dotnet test \
     --filter "Category=Unit&Service=weatherForecast" \
     -p:CollectCoverage=true \
     -p:CoverletOutputFormat=cobertura \
-    -p:CoverletOutput=TestResults/ \
+    -p:CoverletOutput=TestResults/coverage.cobertura.xml \
     -p:Threshold=10 \
-    -p:SkipAutoProps=true \ || exit 1
+    -p:SkipAutoProps=true \
+    --logger "trx;LogFileName=mySampleApp1-wf-tests.unit.trx" || exit 1
 
 # use -p:ThresholdType=line/branch/method for individual thresholds
