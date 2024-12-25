a53e353cdf83dcf555958733c803089886442f6c
Hüseyin Deniz KIVRAK <huseyindenizkivrak@gmail.com>
2024-12-25 13:12:35 +0200

    install rabbitmq for generic usage.

--- terraform/local/redis.tf
+++ terraform/local/redis.tf
@@ -5,6 +5,7 @@ resource "helm_release" "generic_redis" {
   repository       = "oci://registry-1.docker.io/bitnamicharts"
   chart            = "redis"
   create_namespace = true
+  timeout          = 600
 
   values = [file("${path.module}/values/redis-values.yaml")]
 }
