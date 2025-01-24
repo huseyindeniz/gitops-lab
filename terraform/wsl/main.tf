13c9df857150c2bf7fb0a71aec7f285d34ada57c
Hüseyin Deniz KIVRAK <huseyindenizkivrak@gmail.com>
2025-01-24 11:57:48 +0200

    re-arrange shared volume.

--- terraform/wsl/main.tf
+++ terraform/wsl/main.tf
@@ -3,16 +3,33 @@ resource "kubernetes_persistent_volume" "wsl_shared_volume_pv" {
     name = "wsl-shared-volume-pv"
   }
   spec {
-    access_modes = ["ReadWriteOnce"]
+    access_modes = ["ReadWriteMany"]
     capacity = {
       storage = "10Gi"
     }
     persistent_volume_source {
       host_path {
         path = "/mnt/h/volumes/"
+        type = "DirectoryOrCreate"
       }
     }
-    persistent_volume_reclaim_policy = "Delete"
+    persistent_volume_reclaim_policy = "Retain"
     storage_class_name               = "standard"
+  }
+}
+
+resource "kubernetes_persistent_volume_claim" "wsl_shared_volume_pvc" {
+  metadata {
+    name = "wsl-shared-volume-pvc"
+  }
+
+  spec {
+    access_modes = ["ReadWriteMany"]
+
+    resources {
+      requests = {
+        storage = "10Gi"
+      }
+    }
   }
 }
