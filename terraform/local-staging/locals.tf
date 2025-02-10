42aada130d237e06814995d759d5232068f1ff7c
huseyindeniz <huseyindenizkivrak@gmail.com>
2025-02-10 21:32:16 +0200

    fix harbor virtual service.

--- terraform/local-staging/locals.tf
+++ terraform/local-staging/locals.tf
@@ -1,4 +1,4 @@
 locals {
   environments       = jsondecode(kubernetes_config_map.deployment_environments.data["environments"])
-  gitopslab_repo_url = "https://github.com/${var.github_org}/${var.github_repository}.git"
+  gitopslab_repo_url = "https://github.com/${var.github_org}/${var.github_repository}"
 }
