b302863d398c28a551447250d6b5ed8addd68eec
huseyindeniz <huseyindenizkivrak@gmail.com>
2025-02-15 01:02:32 +0200

    move things to argocd attempt.

--- terraform/local-staging/main.tf
+++ terraform/local-staging/main.tf
@@ -39,21 +39,6 @@ module "local_istio" {
   depends_on = [kubernetes_namespace.istio]
 }
 
-resource "kubernetes_manifest" "metallb_configmap_pool" {
-  manifest   = yamldecode(file("${path.module}/manifests/metallb-pool.yaml"))
-  depends_on = [module.local_metallb]
-}
-
-resource "kubernetes_manifest" "metallb_configmap_adv" {
-  manifest   = yamldecode(file("${path.module}/manifests/metallb-adv.yaml"))
-  depends_on = [module.local_metallb]
-}
-
-resource "kubernetes_manifest" "management_cluster_service_entry" {
-  manifest   = yamldecode(file("${path.module}/manifests/management-cluster-service-entry.yaml"))
-  depends_on = [module.local_metallb]
-}
-
 module "local_arc" {
   source          = "../modules/arc-runners"
   name            = "arc-runner-local-staging"
