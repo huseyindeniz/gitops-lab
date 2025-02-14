01aaffc549242898fe7727fa62e2578979057f12
huseyindeniz <huseyindenizkivrak@gmail.com>
2025-02-14 19:08:47 +0200

    move things to argocd attempt.

--- terraform/local-management/argo-management-root.tf
+++ terraform/local-management/argo-management-root.tf
@@ -1,41 +1,7 @@
-# resource "argocd_application" "argo_management_root" {
-#   metadata {
-#     name      = "argo-management-root"
-#     namespace = var.argo_namespace
-#   }
-
-#   spec {
-#     project = "default"
-
-#     source {
-#       repo_url        = local.gitopslab_repo_url
-#       target_revision = "main"
-#       path            = "${var.flux_path}/local-management"
-#       directory {
-#         recurse = true
-#       }
-#     }
-
-#     destination {
-#       server    = "https://kubernetes.default.svc"
-#       namespace = var.argo_namespace
-#     }
-
-#     sync_policy {
-#       automated {
-#         prune     = true
-#         self_heal = true
-#       }
-#     }
-#   }
-
-#   depends_on = [module.local_argo]
-# }
-
 resource "kubernetes_manifest" "argo_management_root" {
   manifest = yamldecode(templatefile("${path.module}/manifests/argo-root.yaml", {
     argo_namespace     = var.argo_namespace
     gitopslab_repo_url = local.gitopslab_repo_url
-    root_path          = "${var.flux_path}/local-management"
+    root_path          = "${var.flux_path}/apps/local-management"
   }))
 }
