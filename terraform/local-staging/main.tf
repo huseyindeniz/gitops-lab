2cca770ec9719f0649206e9e3d4a16f37bbeee2a
huseyindeniz <huseyindenizkivrak@gmail.com>
2025-02-20 19:45:29 +0200

    another istio cross cluster comm fix attempt 898932321323.

--- terraform/local-staging/main.tf
+++ terraform/local-staging/main.tf
@@ -23,18 +23,18 @@ module "local_metallb" {
   depends_on = [kubernetes_namespace.metallb]
 }
 
-resource "kubernetes_manifest" "metallb_ippool" {
-  manifest = yamldecode(templatefile("${path.module}/manifests/IPAddressPool.yaml", {
-    ip_range = "172.17.0.100-172.17.0.149"
-    id       = module.local_metallb.id // fake id for this resource to depend on the module metallb
-  }))
-}
+# resource "kubernetes_manifest" "metallb_ippool" {
+#   manifest = yamldecode(templatefile("${path.module}/manifests/IPAddressPool.yaml", {
+#     ip_range = "172.17.0.100-172.17.0.149"
+#     id       = module.local_metallb.id // fake id for this resource to depend on the module metallb
+#   }))
+# }
 
-resource "kubernetes_manifest" "metallb_l2advertisement" {
-  manifest = yamldecode(templatefile("${path.module}/manifests/L2Advertisement.yaml", {
-    id = module.local_metallb.id // fake id for this resource to depend on the module metallb
-  }))
-}
+# resource "kubernetes_manifest" "metallb_l2advertisement" {
+#   manifest = yamldecode(templatefile("${path.module}/manifests/L2Advertisement.yaml", {
+#     id = module.local_metallb.id // fake id for this resource to depend on the module metallb
+#   }))
+# }
 
 # resource "kubernetes_manifest" "metallb_bgpadvertisement" {
 #   manifest = yamldecode(templatefile("${path.module}/manifests/BGPAdvertisement.yaml", {
@@ -50,14 +50,10 @@ resource "kubernetes_manifest" "metallb_l2advertisement" {
 
 # # ISTIO
 module "local_istio" {
-  source                           = "../modules/istio"
-  istio_namespace                  = kubernetes_namespace.istio.metadata.0.name
-  dns_name                         = "*.staging.local"
-  issuer_name                      = "istio-selfsigned-issuer"
-  tls_secret_name                  = "staging-local-tls-secret"
-  istio_base_values_file           = "${path.module}/values/istio-base.yaml"
-  istiod_values_file               = "${path.module}/values/istio-istiod.yaml"
-  istio_ingressgateway_values_file = "${path.module}/values/istio-ingressgateway.yaml"
+  source                 = "../modules/istio"
+  istio_namespace        = kubernetes_namespace.istio.metadata.0.name
+  istio_base_values_file = "${path.module}/values/istio-base.yaml"
+  istiod_values_file     = "${path.module}/values/istio-istiod.yaml"
 
   providers = {
     kubernetes = kubernetes
@@ -67,26 +63,26 @@ module "local_istio" {
   depends_on = [kubernetes_namespace.istio]
 }
 
-resource "kubernetes_manifest" "istio_gateway" {
-  manifest = yamldecode(templatefile("${path.module}/manifests/istio-gateway.yaml", {
-    istio_namespace = kubernetes_namespace.istio.metadata.0.name
-    tls_secret_name = "staging-local-tls-secret"
-    dns_name        = "*.staging.local"
-    id              = module.local_istio.istioingress_id // fake id for this resource to depend on the module istio
-  }))
-}
+# resource "kubernetes_manifest" "istio_gateway" {
+#   manifest = yamldecode(templatefile("${path.module}/manifests/istio-gateway.yaml", {
+#     istio_namespace = kubernetes_namespace.istio.metadata.0.name
+#     tls_secret_name = "staging-local-tls-secret"
+#     dns_name        = "*.staging.local"
+#     id              = module.local_istio.istioingress_id // fake id for this resource to depend on the module istio
+#   }))
+# }
 
-resource "kubernetes_manifest" "virtual_service_k8sdashboard" {
-  manifest = yamldecode(templatefile("${path.module}/manifests/k8s-dashboard-virtualservice.yaml", {
-    id = module.local_istio.istioingress_id // fake id for this resource to depend on the module istio
-  }))
-}
+# resource "kubernetes_manifest" "virtual_service_k8sdashboard" {
+#   manifest = yamldecode(templatefile("${path.module}/manifests/k8s-dashboard-virtualservice.yaml", {
+#     id = module.local_istio.istioingress_id // fake id for this resource to depend on the module istio
+#   }))
+# }
 
-resource "kubernetes_manifest" "service_entry_harbor" {
-  manifest = yamldecode(templatefile("${path.module}/manifests/service-entry-harbor.yaml", {
-  }))
-  depends_on = [kubernetes_namespace.istio]
-}
+# resource "kubernetes_manifest" "service_entry_harbor" {
+#   manifest = yamldecode(templatefile("${path.module}/manifests/service-entry-harbor.yaml", {
+#   }))
+#   depends_on = [kubernetes_namespace.istio]
+# }
 
 # module "local_arc" {
 #   source          = "../modules/arc-runners"
