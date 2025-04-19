0c8397741483909a45fbdc44cd806aed3f30e8ed
huseyindeniz <huseyindenizkivrak@gmail.com>
2025-04-19 12:41:15 +0300

    install tempo on prod.

--- terraform/local-production/monitoring.tf
+++ terraform/local-production/monitoring.tf
@@ -3,39 +3,3 @@ resource "kubernetes_namespace" "monitoring" {
     name = "monitoring"
   }
 }
-
-# PV
-
-resource "kubernetes_persistent_volume" "prometheus_pv" {
-  metadata {
-    name = "prometheus-pv"
-  }
-  spec {
-    capacity = {
-      storage = "2Gi"
-    }
-    access_modes = ["ReadWriteMany"]
-    persistent_volume_source {
-      host_path {
-        path = "/mnt/data/shared/prometheus"
-        type = "DirectoryOrCreate"
-      }
-    }
-  }
-}
-
-# PVC
-resource "kubernetes_persistent_volume_claim" "prometheus_pvc" {
-  metadata {
-    name      = "prometheus-pvc"
-    namespace = kubernetes_namespace.monitoring.metadata[0].name
-  }
-  spec {
-    access_modes = ["ReadWriteMany"]
-    resources {
-      requests = {
-        storage = "2Gi"
-      }
-    }
-  }
-}
