3ac304405e172b63fdd3ce0d972a4ea77abb0925
huseyindeniz <huseyindenizkivrak@gmail.com>
2025-12-06 20:16:59 +0200

    removing unnecessary flux resources attempt.

--- terraform/local-management/clusters.tf
+++ terraform/local-management/clusters.tf
@@ -1,31 +1,31 @@
 # CLUSTER SECRETS - External cluster configurations for Argo CD
 
 # CLUSTER: local-staging
-# resource "kubernetes_secret" "cluster_local_staging_secret" {
-#   metadata {
-#     name      = "cluster-local-staging-secret"
-#     namespace = kubernetes_namespace.argocd.metadata.0.name
-#     labels = {
-#       "argocd.argoproj.io/secret-type" = "cluster"
-#     }
-#   }
+resource "kubernetes_secret" "cluster_local_staging_secret" {
+  metadata {
+    name      = "cluster-local-staging-secret"
+    namespace = kubernetes_namespace.argocd.metadata.0.name
+    labels = {
+      "argocd.argoproj.io/secret-type" = "cluster"
+    }
+  }
 
-#   type = "Opaque"
+  type = "Opaque"
 
-#   data = {
-#     name   = "cluster-local-staging"
-#     server = "https://172.17.0.5:8443"
-#     config = jsonencode({
-#       bearerToken = var.local_staging_cluster_bearer_token
-#       tlsClientConfig = {
-#         insecure = false
-#         caData   = filebase64("./certs/local-staging.pem")
-#       }
-#     })
-#   }
+  data = {
+    name   = "cluster-local-staging"
+    server = "https://172.17.0.5:8443"
+    config = jsonencode({
+      bearerToken = var.local_staging_cluster_bearer_token
+      tlsClientConfig = {
+        insecure = false
+        caData   = filebase64("./certs/local-staging.pem")
+      }
+    })
+  }
 
-#   depends_on = [module.local_argo]
-# }
+  depends_on = [module.local_argo]
+}
 
 # CLUSTER: local-production
 # resource "kubernetes_secret" "cluster_local_production_secret" {
